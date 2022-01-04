63f97f93ee429364dab021074ee6131d8d4703d5
BonkeG <bonkegcobo291195@gmail.com>
2022-01-04 11:37:52 +0200

    Fix js linter errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import './style.css';
-import logo from './logoImage.jpg'
+import logo from './logoImage.jpg';
 
 const element = document.querySelector('.logoContainer');
 const mylogo = new Image();
 mylogo.className = 'logo';
 mylogo.src = logo;
 
-
 element.append(mylogo);
-
