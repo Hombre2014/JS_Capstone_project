779cee90db21d087b6447cba25d3ddc8a11151a6
BonkeG <bonkegcobo291195@gmail.com>
2022-01-04 11:35:18 +0200

    Add header for the book project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+import logo from './logoImage.jpg'
+
+const element = document.querySelector('.logoContainer');
+const mylogo = new Image();
+mylogo.className = 'logo';
+mylogo.src = logo;
+
+
+element.append(mylogo);
+
